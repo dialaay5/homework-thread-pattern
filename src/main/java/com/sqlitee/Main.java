2618e2f7370c1a567e14d03856769150bb9ac9db
dialaay5 <diala.ay5@gmail.com>
2023-03-29 16:17:24 +0300

    upload all

--- src/main/java/com/sqlitee/Main.java
+++ src/main/java/com/sqlitee/Main.java
@@ -11,20 +11,24 @@ public class Main {
         try
         {
             // 2
-            connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\97250\\Desktop\\full stack\\02.01.2023 express database\\db\\db_rest.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\97250\\Desktop\\database\\person.db");
 
             // 3
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30);
 
             statement.executeUpdate("drop table if exists person");
-            statement.executeUpdate("create table person (id integer, name string)");
-            statement.executeUpdate("insert into person values(1, 'leo')");
-            statement.executeUpdate("insert into person values(2, 'yui')");
+            statement.executeUpdate("create table person (id integer, name string, address string, height float)");
+            statement.executeUpdate("insert into person values(1, 'leo', 'haifa', 1.67)");
+            statement.executeUpdate("insert into person values(2, 'yui', 'ako', 1.87)");
             ResultSet rs = statement.executeQuery("select * from person");
             while (rs.next()) {
+                System.out.format("height = %f \n", rs.getFloat("height"));
+                System.out.format("address = %s \n", rs.getString("address"));
                 System.out.format("name = %s \n", rs.getString("name"));
                 System.out.format("id = %d \n", rs.getInt("id"));
+
+
             }
         }
         catch (SQLException e) {
